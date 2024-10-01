5bcfbc39d6687dfa9b49996431f0809f62cb608f
Petr Gadorek <petr.gadorek@espressif.com>
2024-10-01 14:00:35 +0200

    added esp_idf.json to the settings

--- src/settings.rs
+++ src/settings.rs
@@ -1,13 +1,16 @@
 use config::{Config, ConfigError, File};
 use serde::{Deserialize, Serialize};
+use serde_json::json;
 use std::fs::OpenOptions;
 use std::io::Write;
 use std::path::PathBuf;
+use uuid::Uuid;
 
 #[derive(Debug, Deserialize, Default, Serialize)]
 pub struct Settings {
     pub path: Option<PathBuf>,
     pub idf_path: Option<PathBuf>,
+    pub esp_idf_json_path: Option<String>,
     pub tool_download_folder_name: Option<String>,
     pub tool_install_folder_name: Option<String>,
     pub target: Option<Vec<String>>,
@@ -62,5 +65,85 @@ impl Settings {
             .map_err(|e| ConfigError::Message(e.to_string()))?;
 
         Ok(())
+    }
+
+    pub fn save_esp_ide_json(&self, file_path: &str) -> Result<(), String> {
+        let mut idf_installed = json!({});
+        if let Some(versions) = &self.idf_versions {
+            for version in versions {
+                let id = format!("esp-idf-{}", Uuid::new_v4().to_string().replace("-", "")); //todo: use hash of path or something stable
+                let base_path = self.path.as_ref().unwrap();
+                let idf_path = base_path.clone().join(version).join("esp-idf");
+                let tools_path = base_path
+                    .clone()
+                    .join(version)
+                    .join(self.tool_install_folder_name.as_ref().unwrap());
+                let python_path = match std::env::consts::OS {
+                    "windows" => tools_path
+                        .clone()
+                        .join("python")
+                        .join("Scripts")
+                        .join("Python.exe"),
+                    _ => tools_path
+                        .clone()
+                        .join("python")
+                        .join("bin")
+                        .join("python3"),
+                };
+                let activation_script = match std::env::consts::OS {
+                    "windows" => PathBuf::new(), //TODO: handle windows better
+                    _ => base_path
+                        .clone()
+                        .join(format!("activate_idf_{}.sh", version)),
+                };
+
+                idf_installed[&id] = json!({
+                    "version": version,
+                    "path": idf_path,
+                    "python": python_path,
+                    "idfToolsPath": tools_path,
+                    "activationScript": activation_script,
+                });
+            }
+        }
+
+        let git_path = get_git_path();
+
+        let esp_ide_json = json!({
+          "gitPath": git_path.unwrap(),
+          "idfSelectedId": idf_installed.as_object().unwrap().keys().next().unwrap_or(&String::new()),
+          "idfInstalled": idf_installed,
+      })
+      .to_string();
+        let mut file = OpenOptions::new()
+            .write(true)
+            .truncate(true)
+            .create(true)
+            .open(file_path)
+            .map_err(|e| e.to_string())?;
+        file.write_all(esp_ide_json.as_bytes())
+            .map_err(|e| e.to_string())?;
+
+        Ok(())
+    }
+}
+
+fn get_git_path() -> Result<String, String> {
+    //TODO: move to utils
+    let cmd = match std::env::consts::OS {
+        "windows" => "where",
+        _ => "which",
+    };
+    let output = std::process::Command::new(cmd)
+        .arg("git")
+        .output()
+        .expect("failed to execute process");
+
+    if output.status.success() {
+        let stdout = String::from_utf8_lossy(&output.stdout);
+        Ok(stdout.trim().to_string())
+    } else {
+        let stderr = String::from_utf8_lossy(&output.stderr);
+        Err(stderr.trim().to_string())
     }
 }
