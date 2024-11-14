f0ce05f78eeaa14b1ea4b38555a281eed1f6790d
Hahihula <hahihula@gmail.com>
2024-11-14 14:36:35 +0100

    Eim 67 default setting values (#49)
    * setup all the default values for settings
    
    * EIM-70 parametrized the eim config file location
    
    ---------
    
    Co-authored-by: Petr Gadorek <petr.gadorek@espressif.com>

--- src/settings.rs
+++ src/settings.rs
@@ -1,7 +1,7 @@
 use config::{Config, ConfigError, File};
 use serde::{Deserialize, Serialize};
 use serde_json::json;
-use std::fs::OpenOptions;
+use std::fs::{self, OpenOptions};
 use std::io::Write;
 use std::path::PathBuf;
 use uuid::Uuid;
@@ -21,16 +21,18 @@ pub struct Settings {
     pub tools_json_file: Option<String>,
     pub idf_tools_path: Option<String>,
     pub config_file: Option<PathBuf>,
+    pub config_file_save_path: Option<PathBuf>,
     pub non_interactive: Option<bool>,
     pub wizard_all_questions: Option<bool>,
     pub mirror: Option<String>,
     pub idf_mirror: Option<String>,
     pub recurse_submodules: Option<bool>,
+    pub install_all_prerequisites: Option<bool>,
 }
 
 impl Default for Settings {
     fn default() -> Self {
-        let esp_idf_json_path_value = match std::env::consts::OS {
+        let default_esp_idf_json_path_value = match std::env::consts::OS {
             "windows" => "C:\\Espressif\\tools".to_string(),
             _ => dirs::home_dir()
                 .unwrap()
@@ -40,22 +42,37 @@ impl Default for Settings {
                 .unwrap()
                 .to_string(),
         };
+        let default_path_value = if std::env::consts::OS == "windows" {
+            PathBuf::from(r"C:\esp")
+        } else {
+            PathBuf::from(format!(
+                "{}/.espressif",
+                dirs::home_dir().unwrap().display()
+            ))
+        };
         Self {
-            path: None,
-            idf_path: None,
-            esp_idf_json_path: Some(esp_idf_json_path_value),
+            path: Some(default_path_value),
+            idf_path: None, // TODO: to be removed
+            esp_idf_json_path: Some(default_esp_idf_json_path_value),
             tool_download_folder_name: Some("dist".to_string()),
             tool_install_folder_name: Some("tools".to_string()),
-            target: None,
+            target: Some(vec!["all".to_string()]),
             idf_versions: None,
             tools_json_file: Some("tools/tools.json".to_string()),
             idf_tools_path: Some("tools/idf_tools.py".to_string()),
             config_file: None,
+            config_file_save_path: Some(PathBuf::from("eim_config.toml")),
             non_interactive: Some(false),
             wizard_all_questions: Some(false),
-            mirror: None,
-            idf_mirror: None,
+            mirror: Some(
+                crate::get_idf_tools_mirrors_list()
+                    .first()
+                    .unwrap()
+                    .to_string(),
+            ),
+            idf_mirror: Some(crate::get_idf_mirrors_list().first().unwrap().to_string()),
             recurse_submodules: Some(false),
+            install_all_prerequisites: Some(false),
         }
     }
 }
@@ -88,18 +105,57 @@ impl Settings {
         cfg.try_deserialize()
     }
 
-    pub fn save(&self, file_path: &str) -> Result<(), ConfigError> {
+    pub fn save(&self) -> Result<(), ConfigError> {
+        let mut save_path = self.config_file_save_path.clone().unwrap();
+        if save_path.is_dir() {
+            save_path = save_path.join("eim_config.toml");
+        } else {
+            if let Some(parent) = save_path.parent() {
+                if !parent.exists() {
+                    fs::create_dir_all(parent).unwrap();
+                }
+            }
+        }
         let toml_value = toml::to_string(self).map_err(|e| ConfigError::Message(e.to_string()))?;
         let mut file = OpenOptions::new()
             .write(true)
             .truncate(true)
             .create(true)
-            .open(file_path)
+            .open(save_path)
             .map_err(|e| ConfigError::Message(e.to_string()))?;
         file.write_all(toml_value.as_bytes())
             .map_err(|e| ConfigError::Message(e.to_string()))?;
 
         Ok(())
+    }
+
+    pub fn is_default(&self, field: &str) -> bool {
+        let default_settings = Settings::default();
+        match field {
+            "path" => self.path == default_settings.path,
+            "esp_idf_json_path" => self.esp_idf_json_path == default_settings.esp_idf_json_path,
+            "tool_download_folder_name" => {
+                self.tool_download_folder_name == default_settings.tool_download_folder_name
+            }
+            "tool_install_folder_name" => {
+                self.tool_install_folder_name == default_settings.tool_install_folder_name
+            }
+            "target" => self.target == default_settings.target,
+            "idf_versions" => self.idf_versions == default_settings.idf_versions,
+            "tools_json_file" => self.tools_json_file == default_settings.tools_json_file,
+            "idf_tools_path" => self.idf_tools_path == default_settings.idf_tools_path,
+            "non_interactive" => self.non_interactive == default_settings.non_interactive,
+            "wizard_all_questions" => {
+                self.wizard_all_questions == default_settings.wizard_all_questions
+            }
+            "recurse_submodules" => self.recurse_submodules == default_settings.recurse_submodules,
+            "install_all_prerequisites" => {
+                self.install_all_prerequisites == default_settings.install_all_prerequisites
+            }
+            "mirror" => self.mirror == default_settings.mirror,
+            "idf_mirror" => self.idf_mirror == default_settings.idf_mirror,
+            _ => false,
+        }
     }
 
     /// Saves the ESP IDE configuration as a JSON file to the specified file path.
